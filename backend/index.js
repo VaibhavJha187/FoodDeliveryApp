2a7a0a4e21aa875c4afb4749f6a4584b258d77c2
Vaibhav <vaibhavjha187@gmail.com>
2023-08-17 19:15:28 +0530

    Added cors

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoDB = require("./db")
-
+const cors = require("cors")
+app.use(cors());
 app.use((req,res,next)=>{
   res.header("Access-Control-Allow-Origin","http://localhost:3000");
   res.header(
@@ -15,6 +16,16 @@ app.use((req,res,next)=>{
 
 
 mongoDB(); 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  console.log(req.path, req.method);
+  next();
+});
 app.use(express.json())
 app.use('/api',require("./Routes/CreateUser"));
 app.use('/api',require("./Routes/DisplayData"));
